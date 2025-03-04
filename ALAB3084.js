d310f12fafe883254bcd81f75c1b0e332cb3e393
Sebastian Garcia <198815668+sib2911@users.noreply.github.com>
2025-03-04 08:34:26 -0800

    inital repo creation: part 1 complete, part 2 untouched, part 3 not finished

---
+++ ALAB3084.js
@@ -0,0 +1,55 @@
+//Part 1
+// To begin, solve the following classic “Fizz Buzz” problem.
+// There are a few different ways to do this - experiment with what you think is the most efficient.
+// Once you have solved the problem, ask yourself if there could be another way; and if so, would it be better.
+// Accomplish the following:
+// Loop through all numbers from 1 to 100.
+// If a number is divisible by 3, log “Fizz.”
+// If a number is divisible by 5, log “Buzz.”
+// If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+// If a number is not divisible by either 3 or 5, log the number.
+// Remember to commit your solution once it is working.
+
+
+
+// for (i = 1; i <= 100; i++) {
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         console.log("Fuzz Buzz");
+//         continue;
+//       }
+//   if (i % 3 == 0) {
+//     console.log("fuzz");
+//     continue;
+//   }
+//   if (i % 5 == 0) {
+//     console.log("buzz");
+//     continue;
+//   }
+//   else console.log(i)
+// }
+
+//part3 feeling loopy
+// our task is to write a script that accomplishes the following:
+// Loop through the characters of a given CSV string.
+// Store each “cell” of data in a variable.
+// When you encounter a comma, move to the next cell.
+// When you encounter the “\r\n” sequence, move to the next “row.”
+// Log each row of data.
+// You do not need to format the data, the following works well.
+// console.log(cell1, cell2, cell3, cell4);
+// You can make the following assumptions:
+// There will only be 4 cells per row.
+// There will be no escaped characters other than “\n”.
+// Use the example string provided above to test your program. Once you are confident it is working correctly, try the following string to see if your program works properly with other data.
+// Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232
+
+
+const str = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
+
+for (let i = 0; i < str.length; i++) {
+	if (str[i] == ",") {
+		continue;
+	}
+
+	console.log(str[i]);
+}
